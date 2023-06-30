400be8ec552eb3f0ce3b6452261924a6e69e5dab
邵昀 <137605235+xiaopaodeyun@users.noreply.github.com>
2023-06-30 23:59:02 +0800

    Add files via upload

---
+++ test_2.c/test_2.c/test_2.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+
+
+
+
+	
+		int a = 0;
+		int b = 0;
+		int sum = 0;
+		scanf("%d %d\n", &a, &b);
+		sum = a + b;
+		printf("sum = %d\n", sum);
+
+		return 0;
+	
+}
