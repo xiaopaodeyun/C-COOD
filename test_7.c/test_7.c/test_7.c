e3869095bcc57824806064dc1b416d6ddbd65f55
邵昀 <137605235+xiaopaodeyun@users.noreply.github.com>
2023-07-05 15:19:51 +0800

    Add files via upload

---
+++ test_7.c/test_7.c/test_7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int age = 20;
+	if (age >= 18)
+	{
+		printf("成年\n");
+		printf("想怎么玩就怎么玩\n");
+	}
+	else
+	{
+		printf("未成年\n");
+		printf("不能谈恋爱\n");
+	}
+
+
+
+	return 0;
+}
